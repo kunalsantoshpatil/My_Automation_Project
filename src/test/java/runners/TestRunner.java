d28b35e527d61e7677b25c4a4a84b6b0668a767d
kunal 123 <newgamil@gmail.com>
2025-06-06 13:51:06 +0530

    Latest updated all files

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/resources/features/2login.feature",
+    features = "src/test/resources/features/1Register.feature",
     glue = "stepDefinitions",
     plugin = {
         "pretty",
